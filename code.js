03a8a1880a7576191eb0613094ad2a6a55195a98
Dmitry Verbovetskyi <verbovetskyi.d@gmail.com>
2020-12-21 15:48:03 +0200

    h/w completed

---
+++ code.js
@@ -0,0 +1,33 @@
+//задание 1
+const sum = () => {
+    let i = 0;
+    return (j) => i += j;
+}
+//проверка
+const out = sum();
+console.log(out(3));
+console.log(out(5));
+console.log(out(20));
+
+//задание 2
+function createCounter(initCount = 0, step = 1) {
+    let count = initCount - step;
+    function counter() {
+        return count += step;
+    }
+    counter.reset = function() {
+        count = initCount - step;
+    }
+    return counter;
+}
+//проверка
+const counter = createCounter(5, 2);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+counter.reset();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
